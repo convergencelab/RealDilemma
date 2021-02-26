075bdc20e311d864f77b4e60efe7ee1edafd0a2a
NoahBarrett98 <x2017sre@stfx.ca>
2021-02-26 18:38:28 +0000

    env update

--- src/DRL/gym-pibot/gym_pibot/envs/pibot_env.py
+++ src/DRL/gym-pibot/gym_pibot/envs/pibot_env.py
@@ -8,7 +8,7 @@ ENERGY_THRES = 4000# thres for total amount of different motors used in robo
 MAX_REWARD = 1000
 
 
-class PiBotEnv(gym.Env): 
+class PiBotEnv(gym.Env):
   """
   standard env for PiBot
   this is the most basic implementation of the robotic system.
